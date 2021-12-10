f1a06b25cc82f15b56ccdedb91cb8eb7e239ab09
Rogier van Oudheusden <rogerthat@live.nl>
2021-12-10 12:58:10 +0100

    Add files via upload

---
+++ pizza.js
@@ -0,0 +1,18 @@
+const makePizza = function () {
+    console.log('Make dough');
+    console.log('Add tomato and cheese');
+    console.log('Place in oven');
+};
+
+const makeSushi = function () {
+    console.log('Catch fish');
+    console.log('Boil rice');
+    console.log('Roll sushi');
+};
+
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
